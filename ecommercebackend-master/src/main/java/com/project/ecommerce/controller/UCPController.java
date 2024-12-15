a4be225b4a332a5643aa578ff88aef956602d21b
dmytro.charoian-contractor <dmytro.charoian-contractor@procore.com>
2024-12-15 10:06:51 +0100

    sockets

--- ecommercebackend-master/src/main/java/com/project/ecommerce/controller/UCPController.java
+++ ecommercebackend-master/src/main/java/com/project/ecommerce/controller/UCPController.java
@@ -20,31 +20,31 @@ public class UCPController {
     }
 
     @GetMapping("/v1/tasks")
-    public ResponseEntity<List<Task>> getProducts () {
+    public ResponseEntity<List<Task>> getTasks () {
         log.info("Get All Task Rest Api method called on url: /v1/products" );
         return new ResponseEntity<>(productService.getTasks(), HttpStatus.OK);
     }
 
     @GetMapping("/v1/tasks/{id}")
-    public ResponseEntity<Task> getProduct (@PathVariable("id") Long id) {
+    public ResponseEntity<Task> getTask (@PathVariable("id") Long id) {
         log.info("Get Task Rest Api method called on url: /v1/products/{id}" );
         return new ResponseEntity<>(productService.getTask(id), HttpStatus.OK);
     }
 
     @PostMapping("/v1/tasks")
-    public ResponseEntity<Task> addProduct (@RequestBody Task task) {
+    public ResponseEntity<Task> addTask (@RequestBody Task task) {
         log.info("Add Task Rest Api method called on url: /v1/products" );
         return new ResponseEntity<>(productService.addTask(task), HttpStatus.OK);
     }
 
     @PatchMapping("/v1/tasks/{id}")
-    public ResponseEntity<Task> updateProduct (@PathVariable("id") Long id, @RequestBody Task task) {
+    public ResponseEntity<Task> updateTask (@PathVariable("id") Long id, @RequestBody Task task) {
         log.info("Update Task Rest Api method called on url: /v1/products/{id}" );
         return new ResponseEntity<>(productService.updateTask(id, task), HttpStatus.OK);
     }
 
     @DeleteMapping("/v1/tasks/{id}")
-    public ResponseEntity<?> deleteProduct (@PathVariable("id") Long id) {
+    public ResponseEntity<?> deleteTask (@PathVariable("id") Long id) {
         log.info("Delete Task Rest Api method called on url: /v1/products/{id}" );
         productService.deleteTask(id);
         return new ResponseEntity<>(HttpStatus.OK);
