a04197c82e4f21091492ad0195496f928e278806
dmytro.charoian-contractor <dmytro.charoian-contractor@procore.com>
2024-12-17 09:28:35 +0100

    auto send socket

--- ecommercebackend-master/src/main/java/com/project/ecommerce/service/ChatWebSocketHandler.java
+++ ecommercebackend-master/src/main/java/com/project/ecommerce/service/ChatWebSocketHandler.java
@@ -1,23 +1,41 @@
 package com.project.ecommerce.service;
 
+import com.project.ecommerce.entity.Task;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+@Component
 public class ChatWebSocketHandler extends TextWebSocketHandler {
 
     private final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session); // Add session to active connections
         System.out.println("New connection: " + session.getId());
+        scheduler.scheduleAtFixedRate(() -> sendDeadlineNotification(), 0, 1, TimeUnit.MINUTES);
     }
 
     @Override
@@ -37,5 +55,34 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         sessions.remove(session); // Remove session on disconnect
         System.out.println("Connection closed: " + session.getId());
+    }
+
+    public void sendDeadlineNotification() {
+        String message = "Deadline on nearest task: " + generateTaskContent();
+        System.out.println("Sending message: " + message);
+
+        synchronized (sessions) {
+            System.out.println("Active sessions before send: " + sessions.size());
+
+            for (WebSocketSession session : sessions) {
+                if (session.isOpen()) {
+                    try {
+                        session.sendMessage(new TextMessage(message));
+                        System.out.println("Message sent to session: " + session.getId());
+                    } catch (Exception e) {
+                        System.err.println("Error sending message: " + e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+    @Getter
+    List<com.project.ecommerce.entity.Task> tasks = new ArrayList<>() {{
+        add(new com.project.ecommerce.entity.Task(1L, "Learn Vue.js",true, new Date()));
+        add(new Task(2L, "Build a TODO App", false, new Date()));
+    }};
+    private String generateTaskContent() {
+        Date now = tasks.get(0).getDeadline();
+        return now.toString();
     }
 }
--- ecommercebackend-master/src/main/java/com/project/ecommerce/service/WebSocket.java
+++ ecommercebackend-master/src/main/java/com/project/ecommerce/service/WebSocket.java
@@ -1,24 +1,16 @@
 package com.project.ecommerce.service;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocket implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocket implements WebSocketConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:8081", "https://example.com/");
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:8081", "https://example.com/").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ChatWebSocketHandler(), "/chat").setAllowedOrigins("*");
     }
 }
